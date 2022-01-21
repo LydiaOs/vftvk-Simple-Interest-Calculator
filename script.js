dcdd349a73578fe32c9a3822de85c60f58f21fc6
LydiaOs <89894269+LydiaOs@users.noreply.github.com>
2022-01-21 09:24:35 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,33 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    if (!principal || principal < 1)
+    {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = parseFloat(principal) * parseFloat(years) * parseFloat(rate) / 100;
+    var year = new Date().getFullYear() + parseInt(years);
+    var outtext = "If you deposit <span class='number'>" + principal + "</span>,</br>";
+    outtext += "at an interest rate of <span class='number'>" + rate + "</span>%.</br>";
+    outtext += "You will receive and amount of <span class='number'>" + interest + "</span>,</br>"
+    outtext += "in the year <span class='number'>" + year + "</span>";
+    document.getElementById("result").innerHTML = outtext;
 }
-        
+      
+function updateRate()
+{
+    if (document.getElementById("rate")) {
+        var reateval = document.getElementById("rate").value;
+       document.getElementById("rate_val").innerText = reateval;
+}
+
+}
+
+window.onload = function ()
+{
+  //  document.getElementById("rate").onchange = updateRate();
+}       
